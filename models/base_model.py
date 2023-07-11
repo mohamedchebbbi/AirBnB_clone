f0b2f1a6ecd110ba09c9741091320d38dd03b3ad
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-07-11 10:54:26 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
+
 import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """Base model class for other classes."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel instance."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
-            class_name = self.__class__.__name__
-            return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        """Return the string representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-            self.updated_at = datetime.now()
+        """Update the public instance attribute updated_at with the current datetime."""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     def test_init(self):
         my_model = BaseModel()
         self.assertIsNotNone(my_model.id)
         self.assertIsNotNone(my_model.created_at)
-        self.assertIsNotNone(my_model.updated_at)
 
     def test_str(self):
         my_model = BaseModel()
@@ -22,12 +23,35 @@ class TestBaseModel(unittest.TestCase):
 
     def test_to_dict(self):
         my_model = BaseModel()
-        my_model.name = "My First Model"
+        my_model.name = "My_First_Model"
         my_model.my_number = 89
         obj_dict = my_model.to_dict()
         self.assertIsInstance(obj_dict, dict)
         self.assertEqual(obj_dict['__class__'], 'BaseModel')
-        self.assertEqual(obj_dict['name'], 'My First Model')
+        self.assertEqual(obj_dict['name'], 'My_First_Model')
         self.assertEqual(obj_dict['my_number'], 89)
         self.assertIn('created_at', obj_dict)
         self.assertIn('updated_at', obj_dict)
+
+        created_at_str = obj_dict['created_at']
+        created_at_dt = datetime.strptime(created_at_str, '%Y-%m-%dT%H:%M:%S.%f')
+        self.assertIsInstance(created_at_dt, datetime)
+        self.assertEqual(created_at_dt, my_model.created_at)
+
+    def test_from_dict(self):
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+
+        my_new_model = BaseModel(**my_model_json)
+
+        self.assertEqual(my_model.id, my_new_model.id)
+        self.assertEqual(my_model.created_at, my_new_model.created_at)
+        self.assertEqual(my_model.updated_at, my_new_model.updated_at)
+        self.assertEqual(my_model.name, my_new_model.name)
+        self.assertEqual(my_model.my_number, my_new_model.my_number)
+        self.assertIsInstance(my_new_model.created_at, datetime)
+
+if __name__ == '__main__':
+    unittest.main()
